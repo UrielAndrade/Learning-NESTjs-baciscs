5edfddb337e94f68935214c1c58f57664a2de12f
UrielAndrade <andradeleiru@gmail.com>
2025-07-19 13:18:32 -0300

    new rout find by id (incomplete)

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
   @Get()
   findAll(){
     return 'listagem de cursos';
+  }
+  @Get(':id')
+  findOne(@Param() params) {
+    return `Curso com ID ${params.id}`;
   }
 }
