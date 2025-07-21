b92fb276f7678ee16a920fefe757f44219e1d868
UrielAndrade <andradeleiru@gmail.com>
2025-07-20 22:57:01 -0300

    new feat

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,19 +1,44 @@
-import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { CoursesService } from 'src/courses/courses.service';
 
 @Controller('courses')
 export class CoursesController {
+  constructor(private readonly coursesService: CoursesService) {}
   @Get()
-  findAll(@Res() response){
-    return response.status(200).json({message: 'Lista de cursos'});
+  findAll(@Res() response) {
+    return response.status(200).json({ message: 'Lista de cursos' });
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `Curso com ID ${id}`;
   }
-  
+
   @Post()
-  create(@Body() body){
+  create(@Body() body) {
     return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    console.log(body);
+    return `Atualizar o curso com ID ${id}`;
+  }
+
+  @HttpCode(HttpStatus.BAD_GATEWAY)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `Remover o curso com ID ${id}`;
   }
 }
